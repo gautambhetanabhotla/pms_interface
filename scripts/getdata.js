5673094a7cf756fd9c6cb4b7972c8c57f81898c3
gautambhetanabhotla <143905036+gautambhetanabhotla@users.noreply.github.com>
2024-04-21 01:46:52 +0530

    Charting figured out, kinda

--- scripts/getdata.js
+++ scripts/getdata.js
@@ -1,9 +1,9 @@
-let numResults = 10;
-let numFields = 1;
+let numResults = 7, numFields = 1;
 var APIKEY = null;
 var channelID = null;
 let TSrequest = "https://api.thingspeak.com/channels/" + channelID + "/feeds.json?api_key=" + APIKEY + "&results=" + numResults;
 
+// fetch thingspeak data (API key and channel ID) from ./private/Thingspeak.json and assign them to the variables
 fetch("private/Thingspeak.json").then(response => response.json()).then(data => {
     console.log(data);
     channelID = data["channelID"];
---
+++ scripts/showdata.js
@@ -0,0 +1,57 @@
+var labels = [];
+for(var i = 0; i < numResults; i++) {
+    labels.push("");
+}
+
+var data = [65, 59, 80, 81, 56, 55, 40];
+
+console.log(labels);
+
+var hrdata = {
+    labels: labels,
+    datasets: [{
+        label: 'Heart rate',
+        data: data,
+        fill: false,
+        borderColor: 'rgb(0, 236, 169)',
+        tension: 0
+    }]
+};
+
+var hr = document.getElementById("heartRate");
+new Chart(hr, {
+    type: 'line',
+    data: hrdata,
+    options: {
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'rgb(255, 255, 255)' // Change the color to red
+                }
+            },
+            title: {
+                display: false,
+                text: 'IOTIOTIOTIOT',
+                color: 'rgb(255, 0, 0)' // Change the color to red
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: 'rgb(255, 0, 0)' // Change the color to red
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: 'rgb(255, 0, 0)' // Change the color to red
+                    }
+                }
+            }
+        }
+    }
+});
+
+var bp = document.getElementById("BP");
+new Chart(bp, {
+    type: 'line',
+    data: hrdata
+})
